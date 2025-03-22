8f34900c0a34e75d1b3242928ba1b2593b653e08
ggjorven <Jorbenvanderwal@gmail.com>
2025-03-22 15:15:19 +0100

    Try a fix for Fuse enums.

--- Lunar/src/Lunar/Internal/Enum/Fuse.hpp
+++ Lunar/src/Lunar/Internal/Enum/Fuse.hpp
@@ -9,6 +9,8 @@
 
 #include "Lunar/Internal/Maths/Logarithm.hpp"
 
+#include "Lunar/Internal/Utils/Hash.hpp"
+
 namespace Lunar::Internal::Enum
 {
 
@@ -18,8 +20,10 @@ namespace Lunar::Internal::Enum
     template<typename TEnum> requires(std::is_enum_v<TEnum>)
     constexpr uintmax_t FuseOne(uintmax_t hash, TEnum value)
     {
-        size_t index = Index(value);
-        return (hash << Lunar::Internal::Log2((Count<TEnum>() << 1) - 1)) | index;
+        constexpr uintmax_t typeHash = static_cast<uintmax_t>(typeid(TEnum).hash_code());
+        constexpr uintmax_t integerValue = static_cast<uintmax_t>(value);
+
+        return (hash * 31) ^ (typeHash + integerValue);
     }
 
     template<typename TEnum> requires(std::is_enum_v<TEnum>)
